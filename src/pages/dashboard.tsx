b6fc087888a04980be7bc0405b572e83b9906b31
Ashis Kumar <ashishsahoo389@gmail.com>
2025-02-26 17:03:28 +0530

    remove unused import.

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { firebaseConfig } from '../config/firebase';
 import TaskCard from '@/components/TaskCard';
 import Logo from '@/components/Logo';
